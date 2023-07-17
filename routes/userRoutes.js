7c7def28c8f36b2e2c5acb17d5aa3ea8f44a8c60
akestler <andrew.kestler@generalassemb.ly>
2023-07-17 10:13:47 -0400

    userRoutes written and hooked up to socket
    tweaked model for token to allow null by default
    
    wrote user routes for socket connection largely as parallels of the traditional express pattern
    
    reworked an existing errorHandler for express to
    work with my current socket pattern and included some custom error messaging via classes
    
    registered routes in server.js
    
    *untested*
    
    Considering a custom isSignedIn auth middle ware to detect and handle finding the user associated with the request by default for any authenticated 'routes'
    
    - will likely require a minor refactor of the way the current routes are configured.

---
+++ routes/userRoutes.js
@@ -0,0 +1,119 @@
+const User = require('../models/user')
+const {
+	BadParamsError,
+	DocumentNotFoundError,
+	BadCredentialsError,
+} = require('../lib/custom_errors')
+const bcrypt = require('bcrypt')
+const bcryptSaltRounds = 10
+
+const routerObj = {} // declare my router pojo
+
+routerObj['signup'] = async (data, io, socketId, next) => {
+	console.log('in signup route before user creation', data)
+	try {
+		if (
+			// check redundant with FE JS but i don't see that as a bad thing
+			!data ||
+			!data.password ||
+			!data.profileName ||
+			data.password !== data.passwordConfirmation
+		) {
+			throw new BadParamsError()
+		}
+        // we have good params
+        // hash the password
+		const hashPassword = await bcrypt.hash(data.password, bcryptSaltRounds)
+        // get the other details
+		let { email, profileName } = data
+        // build our data object for creation 
+		let userData = { email, profileName, hashPassword }
+		// userData.email = data.email,
+		// userData.hashedPassword = hash
+		let user = await User.create(userData)
+		// send the new user object back with status 201, but `hashedPassword`
+		// won't be send because of the `transform` in the User model
+        // removed the email and hash password from the data for sending to client 
+		user = await user.toObject()
+		io.to(socketId).emit('signupSuccess', user) 
+        // add auto sign in functionality here
+	} catch (error) {
+		next(error, io, socketId) // promisedError, io, socketId
+	}
+}
+
+routerObj['signin'] = async (data, io, socketId, next) => {
+	console.log('in userRouter for signin, the data passed is :', data)
+	const pw = data.password
+	try {
+		const user = await User.findOne({ email: data.email })
+		if (!user) {
+			throw new BadCredentialsError()
+		}
+		console.log(user)
+		const passwordCorrect = await bcrypt.compare(pw, user.hashPassword)
+		if (!passwordCorrect) {
+			throw new BadCredentialsError()
+		}
+		const token = crypto.randomBytes(16).toString('hex')
+		user.token = token
+		user.socketId = socketId
+		await user.save()
+		io.to(socketId).emit('signinSuccess', user.toObject())
+	} catch (error) {
+		next(error, io, socketId)
+	}
+}
+
+routerObj['changePassword'] = async (data, io, socketId, next) => {
+	console.log(
+		'in change password route, the data passed is',
+		data,
+		'from socket id:',
+		socketId
+	)
+	const pw = data.password
+	const newPw = data.newPassword
+    const token = data.token
+    const userId = data._id
+	try {
+		const user = await User.findById(userId)
+		if (!user) {
+			throw new DocumentNotFoundError()
+		}
+        if (!user.token === token){
+            throw new BadCredentialsError()
+        }
+		const passwordCorrect = await bcrypt.compare(pw, user.hashPassword)
+		if (!passwordCorrect) {
+			throw new BadCredentialsError()
+		}
+		const hashPassword = await bcrypt.hash(newPw, bcryptSaltRounds)
+		user.hashPassword = hashPassword
+		await user.save()
+		io.to(socketId).emit('changePasswordSuccess')
+	} catch (error) {
+		next(error, io, socketId)
+	}
+}
+
+routerObj['signout'] = async (data, io, socketId, next) => {
+	console.log('in signout route, data is :', data)
+	try {
+		const user = await User.findById(data._id)
+		if (!user) {
+            throw new DocumentNotFoundError()
+        }
+        // scramble the token
+        const token = crypto.randomBytes(16).toString('hex')
+        user.token = token
+        // clear the socket - no more emits going here
+        user.socketId = null
+        await user.save()
+		io.to(socketId).emit('signoutSuccess')
+	} catch (error) {
+		next(error, io, socketId)
+	}
+}
+
+module.exports = routerObj
--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const http = require('http')
 const server = http.createServer(app)
 // express basics above
 
+// other imports
+const {
+	signup,
+	signin,
+	signout,
+	changePassword,
+} = require('./routes/userRoutes')
+const errorHandler = require('./lib/errorHandler')
+
 //db connection in between
 require('dotenv').config()
 require('./config/database')
@@ -17,8 +26,32 @@ const io = new Server(server,{
     }
 })
 
+
+
 io.on('connection', (socket) => {
 	console.log('a user connected with socket:', socket.id)
+    
+    socket.on('signup', (data) => {
+			console.log(' a user signed up ')
+			console.log(data)
+			signup(data, io, socket.id, errorHandler)
+    })
+    socket.on('signin', (data) => {
+        console.log(' a user signed in ')
+        console.log(data)
+        signin(data, io, socket.id, errorHandler)
+    })
+    // should write a is logged in middleware to protect auth required routes
+    socket.on('changePassword', (data) => {
+        console.log('received change pw request')
+        changePassword(data, io, socket.id, errorHandler)
+	})
+
+	socket.on('signout', (data) => {
+		signout(data, io, socket.id, errorHandler)
+		// console.log(socket.rooms)// interesting being able to get the socket rooms left on disconnect even tho cleanup is automated - will be great for trigger appropriate emits to those rooms 
+		console.log(' a user signed out')
+    })
     
     socket.on('disconnect', () => {
         console.log('user disconnected with socket:', socket.id)
